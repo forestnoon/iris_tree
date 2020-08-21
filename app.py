9ebbace07642809a19b0a185f2aafeb052f902c9
forestnoon <gen08350835@gmail.com>
2020-08-21 14:34:53 +0900

    master

--- app.py
+++ app.py
@@ -1,12 +1,28 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, url_for
 from sklearn.tree import DecisionTreeClassifier
 import numpy as np
 import joblib
 from form import iris_form
+import os
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'secret key'
 app.config['WTF_CSRF_ENABLED'] = True
+
+# ブラウザのキャッシュを更新させる関数
+@app.context_processor
+def override_url_for():
+    return dict(url_for=dated_url_for)
+
+def dated_url_for(endpoint, **values):
+    if endpoint == 'static':
+        filename = values.get('filename', None)
+        if filename:
+            file_path = os.path.join(app.root_path,
+                                     endpoint, filename)
+            values['q'] = int(os.stat(file_path).st_mtime)
+    return url_for(endpoint, **values)
+
 
 @app.route('/')
 def index():
@@ -50,6 +66,6 @@ def tree():
 
 
 if __name__ == '__main__':
-    app.run()
-    # app.debug = True
-    # app.run(host='localhost')
+    # app.run()
+    app.debug = True
+    app.run(host='localhost')
